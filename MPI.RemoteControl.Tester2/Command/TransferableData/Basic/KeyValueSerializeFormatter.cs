21395fff1c0ad4f7751debc949a200848c534ef0
David Kao <David.Kao@mpi.com.tw>
2020-10-08 09:00:24 +0800

    更新 KeyValueSerializeFormatter 無法處理 bool的問題

--- MPI.RemoteControl.Tester2/Command/TransferableData/Basic/KeyValueSerializeFormatter.cs
+++ MPI.RemoteControl.Tester2/Command/TransferableData/Basic/KeyValueSerializeFormatter.cs
@@ -14,10 +14,57 @@ namespace MPI.RemoteControl2.Tester.Mpi.Command.Base
 
 		#endregion
 
+		#region >>> Public static method <<<
+
+		/// <summary>
+		/// Convert byte array to string.
+		/// </summary>
+		public static string ConvertToString( byte[] buffer )
+		{
+			string temp = "";
+
+			for ( int nIndex = 0; nIndex < buffer.Length; ++nIndex )
+			{
+				temp += String.Format( "{0}", buffer[ nIndex ].ToString( "X2" ) );
+			}
+
+			return temp;
+		}
+
+		/// <summary>
+		/// Convert string to byte array.
+		/// </summary>
+		public static byte[] ConvertToBytes( string buffer )
+		{
+			List<byte> temp = new List<byte>();
+
+			buffer = buffer.PadRight( buffer.Length % 2 + buffer.Length, '\0' );
+
+			int value = 0;
+			for ( int nIndex = 0; nIndex < buffer.Length; nIndex += 2 )
+			{
+				if ( Int32.TryParse( buffer.Substring( nIndex, 2 ), System.Globalization.NumberStyles.HexNumber, null, out value ) )
+				{
+					temp.Add( ( byte ) value );
+				}
+			}
+
+			return temp.ToArray();
+		}
+
+		#endregion
+
 		#region >>> Public method <<<
 
+		/// <summary>
+		/// Converts the value of a specified object to an equivalent string representation using 
+		/// specified format and culture-specific formatting information.
+		/// </summary>
 		public string Format(string format, object arg, IFormatProvider formatProvider)
 		{
+			if ( arg == null )
+				return "";
+
 			if (arg is double || arg is float)
 			{
 				return ConvertToString(BitConverter.GetBytes((dynamic)arg));
@@ -31,65 +78,38 @@ namespace MPI.RemoteControl2.Tester.Mpi.Command.Base
 			{
 				return (string)arg;
 			}
-			else if (arg.GetType().IsSerializable)
-			{
-				using (MemoryStream ms = new MemoryStream())
-				{
-					try
-					{
-						_formatter.Serialize(ms, arg);
+            else if (arg is bool)
+            {
+                return arg.ToString();
+            }
+            else if (arg.GetType().IsSerializable)
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    try
+                    {
+                        _formatter.Serialize(ms, arg);
 
-						return ConvertToString(ms.ToArray());
-					}
-					catch
-					{
-						return "";
-					}
-				}
-			}
-			else
-			{
-				return arg.ToString();
-			}
+                        return ConvertToString(ms.ToArray());
+                    }
+                    catch
+                    {
+                        return "";
+                    }
+                }
+            }
+            else
+            {
+                return arg.ToString();
+            }
 		}
 
+		/// <summary>
+		/// Returns an object that provides formatting services for the specified type.
+		/// </summary>
 		public object GetFormat(Type formatType)
 		{
 			return (formatType == typeof(ICustomFormatter)) ? this : null;
-		}
-
-		#endregion
-
-		#region >>> Public static method <<<
-
-		public static string ConvertToString(byte[] buffer)
-		{
-			string temp = "";
-
-			for (int nIndex = 0; nIndex < buffer.Length; ++nIndex)
-			{
-				temp += String.Format("{0}", buffer[nIndex].ToString("X2"));
-			}
-
-			return temp;
-		}
-
-		public static byte[] ConvertToBytes(string buffer)
-		{
-			List<byte> temp = new List<byte>();
-
-			buffer = buffer.PadRight(buffer.Length % 2 + buffer.Length, '\0');
-
-			int value = 0;
-			for (int nIndex = 0; nIndex < buffer.Length; nIndex += 2)
-			{
-				if (Int32.TryParse(buffer.Substring(nIndex, 2), System.Globalization.NumberStyles.HexNumber, null, out value))
-				{
-					temp.Add((byte)value);
-				}
-			}
-
-			return temp.ToArray();
 		}
 
 		#endregion
